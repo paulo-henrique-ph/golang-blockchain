b665c773391901fb30904344fa11ccfaace5b8d2
Paulo Henrique Gomes Pinto <paulo.henrique@kumulus.com.br>
2022-03-07 18:20:25 -0300

    Refactored to use modules

---
+++ blockchain/block.go
@@ -0,0 +1,45 @@
+package blockchain
+
+import (
+	"bytes"
+	"crypto/sha256"
+)
+
+type BlockChain struct {
+	Blocks []*Block
+}
+
+type Block struct {
+	Hash         []byte
+	Data         []byte
+	PreviousHash []byte
+}
+
+// DeriveHash makes a new hash based
+func (b *Block) DeriveHash() {
+	info := bytes.Join([][]byte{b.Data, b.PreviousHash}, []byte{})
+	hash := sha256.Sum256(info)
+	b.Hash = hash[:]
+}
+
+// Create a block on the chain
+// returns the block
+func CreateBlock(data string, previousHash []byte) *Block {
+	block := &Block{[]byte{}, []byte(data), previousHash}
+	block.DeriveHash()
+	return block
+}
+
+func (chain *BlockChain) AddBlock(data string) {
+	previousBlock := chain.Blocks[len(chain.Blocks)-1]
+	newBlock := CreateBlock(data, previousBlock.Hash)
+	chain.Blocks = append(chain.Blocks, newBlock)
+}
+
+func Genesis() *Block {
+	return CreateBlock("Genesis", []byte{})
+}
+
+func InitBlockChain() *BlockChain {
+	return &BlockChain{[]*Block{Genesis()}}
+}
--- main.go
+++ main.go
@@ -1,58 +1,19 @@
 package main
 
 import (
-	"bytes"
-	"crypto/sha256"
 	"fmt"
+
+	"github.com/paulo-henrique-ph/golang-blockchain/blockchain"
 )
 
-type BlockChain struct {
-	blocks []*Block
-}
-
-type Block struct {
-	Hash         []byte
-	Data         []byte
-	PreviousHash []byte
-}
-
-// DeriveHash makes a new hash based
-func (b *Block) DeriveHash() {
-	info := bytes.Join([][]byte{b.Data, b.PreviousHash}, []byte{})
-	hash := sha256.Sum256(info)
-	b.Hash = hash[:]
-}
-
-// Create a block on the chain
-// returns the block
-func CreateBlock(data string, previousHash []byte) *Block {
-	block := &Block{[]byte{}, []byte(data), previousHash}
-	block.DeriveHash()
-	return block
-}
-
-func (chain *BlockChain) AddBlock(data string) {
-	previousBlock := chain.blocks[len(chain.blocks)-1]
-	newBlock := CreateBlock(data, previousBlock.Hash)
-	chain.blocks = append(chain.blocks, newBlock)
-}
-
-func Genesis() *Block {
-	return CreateBlock("Genesis", []byte{})
-}
-
-func InitBlockChain() *BlockChain {
-	return &BlockChain{[]*Block{Genesis()}}
-}
-
 func main() {
-	chain := InitBlockChain()
+	chain := blockchain.InitBlockChain()
 
 	chain.AddBlock("1")
 	chain.AddBlock("2")
 	chain.AddBlock("3")
 
-	for _, block := range chain.blocks {
+	for _, block := range chain.Blocks {
 		fmt.Printf("Previous Hash: %s\n", block.PreviousHash)
 		fmt.Printf("Data in block: %s\n", block.Data)
 		fmt.Printf("Hash: %s\n", block.Hash)

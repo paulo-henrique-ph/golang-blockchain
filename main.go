0f78051f34a98b90c19dfbc1a361b614a06c3940
Paulo Henrique Gomes Pinto <paulo.henrique@kumulus.com.br>
2022-03-07 13:24:17 -0300

    Implemented the block constructor

--- main.go
+++ main.go
@@ -1,9 +1,29 @@
 package main
 
+import (
+	"bytes"
+	"crypto/sha256"
+)
+
 type Block struct {
 	Hash         []byte
 	Data         []byte
 	PreviousHash []byte
+}
+
+// DeriveHash makes a new hash based
+func (b *Block) DeriveHash() {
+	info := bytes.Join([][]byte{b.Data, b.PreviousHash}, []byte{})
+	hash := sha256.Sum256(info)
+	b.Hash = hash[:]
+}
+
+// Create a block on the chain
+// returns the block
+func CreateBlock(data string, previousHash []byte) *Block {
+	block := &Block{[]byte{}, []byte(data), previousHash}
+	block.DeriveHash()
+	return block
 }
 
 func main() {

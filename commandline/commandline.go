8d23cba379716fb547715f8bd6c5e775acbc4403
Paulo Henrique Gomes Pinto <paulo.henrique@kumulus.com.br>
2022-03-09 09:32:06 -0300

    Added CLI to the project

---
+++ commandline/commandline.go
@@ -0,0 +1,85 @@
+package commandline
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"runtime"
+	"strconv"
+
+	"github.com/paulo-henrique-ph/golang-blockchain/blockchain"
+)
+
+type CommandLine struct {
+	Blockchain *blockchain.BlockChain
+}
+
+func (cli *CommandLine) printUsage() {
+	fmt.Println("Usage:")
+	fmt.Println("add -block BLOCK_DATA - add a block to the chain")
+	fmt.Println("print - Prints the blocks in the chain")
+}
+
+func (cli *CommandLine) validateArgs() {
+	if len(os.Args) < 2 {
+		cli.printUsage()
+		runtime.Goexit()
+	}
+}
+
+func (cli *CommandLine) addBlock(data string) {
+	cli.Blockchain.AddBlock(data)
+	fmt.Println("Added block!")
+}
+
+func (cli *CommandLine) printChain() {
+	iter := cli.Blockchain.Iterator()
+
+	for {
+		block := iter.Next()
+
+		fmt.Printf("Previous Hash: %x\n", block.PreviousHash)
+		fmt.Printf("Data in block: %s\n", block.Data)
+		fmt.Printf("Hash: %x\n", block.Hash)
+
+		pow := blockchain.NewProof(block)
+		fmt.Printf("PoW: %s\n", strconv.FormatBool(pow.Validate()))
+
+		if len(block.PreviousHash) == 0 {
+			break
+		}
+	}
+}
+
+func (cli *CommandLine) Run() {
+	cli.validateArgs()
+
+	addBlockCmd := flag.NewFlagSet("add", flag.ExitOnError)
+	printChainCmd := flag.NewFlagSet("print", flag.ExitOnError)
+	addBlockData := addBlockCmd.String("block", "", "Block Data")
+
+	switch os.Args[1] {
+	case "add":
+		err := addBlockCmd.Parse(os.Args[2:])
+		blockchain.Handle(err)
+	case "print":
+		err := printChainCmd.Parse(os.Args[2:])
+		blockchain.Handle(err)
+	default:
+		cli.printUsage()
+		runtime.Goexit()
+	}
+
+	if addBlockCmd.Parsed() {
+		if *addBlockData == "" {
+			addBlockCmd.Usage()
+			runtime.Goexit()
+		}
+
+		cli.addBlock(*addBlockData)
+	}
+
+	if printChainCmd.Parsed() {
+		cli.printChain()
+	}
+}

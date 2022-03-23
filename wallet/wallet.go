dbf3f6ce9e24f0c91a2cfa5d1e9e46d7324f5cf6
Paulo Henrique Gomes Pinto <paulo.henrique@kumulus.com.br>
2022-03-23 07:42:17 -0300

    Added wallets modules

---
+++ wallet/wallet.go
@@ -0,0 +1,78 @@
+package wallet
+
+import (
+	"crypto/ecdsa"
+	"crypto/elliptic"
+	"crypto/rand"
+	"crypto/sha256"
+	"fmt"
+	"log"
+
+	"golang.org/x/crypto/ripemd160"
+)
+
+const (
+	checksumLength = 4
+	version        = byte(0x00)
+)
+
+type Wallet struct {
+	PriveteKey ecdsa.PrivateKey
+	PublicKey  []byte
+}
+
+func (wallet Wallet) Address() []byte {
+	pubHash := PublicKeyHash(wallet.PublicKey)
+
+	versionedHash := append([]byte{version}, pubHash...)
+	checksum := Checksum(versionedHash)
+
+	fullHash := append(versionedHash, checksum...)
+	address := Base58Encode(fullHash)
+
+	fmt.Printf("public key: %x\n", wallet.PublicKey)
+	fmt.Printf("public hash: %x\n", pubHash)
+	fmt.Printf("address: %x\n", address)
+
+	return address
+}
+
+func NewKeyPair() (ecdsa.PrivateKey, []byte) {
+	curve := elliptic.P256()
+
+	private, err := ecdsa.GenerateKey(curve, rand.Reader)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	pub := append(private.PublicKey.X.Bytes(), private.Y.Bytes()...)
+	return *private, pub
+}
+
+func MakeWallet() *Wallet {
+	private, public := NewKeyPair()
+	wallet := Wallet{private, public}
+
+	return &wallet
+}
+
+func PublicKeyHash(pubKey []byte) []byte {
+	pubHash := sha256.Sum256(pubKey)
+
+	hasher := ripemd160.New()
+	_, err := hasher.Write(pubHash[:])
+	if err != nil {
+		log.Panic(err)
+	}
+
+	publicRipMD := hasher.Sum(nil)
+
+	return publicRipMD
+}
+
+func Checksum(payload []byte) []byte {
+	firstHash := sha256.Sum256(payload)
+	secondHash := sha256.Sum256(firstHash[:])
+
+	return secondHash[:checksumLength]
+}

0670cd2d992848465135f7453945ba2642cd18b7
paulo-henrique-ph <paulohgp343@gmail.com>
2024-11-09 08:14:42 -0300

    Add reindexutxo command for rebuilding UTXO set
    Introduce the `reindexutxo` command to the command-line interface, enabling users to rebuild the UTXO set. Updated relevant functions and modified the README with usage instructions for the new command.

--- README.md
+++ README.md
@@ -1,2 +1,47 @@
 # golang-blockchain
 Simple Blockchain implementation in go
+
+
+## Usage
+
+### Get Balance
+Retrieve the balance for a specific address.
+```sh
+getbalance -address ADDRESS
+```
+
+### Create Blockchain
+Create a new blockchain and send a reward to the specified address.
+```sh
+createblockchain -address ADDRESS
+```
+
+### Print Chain
+Print all the blocks in the blockchain.
+```sh
+printchain
+```
+
+### Send Coins
+Send a specified amount of coins from one address to another.
+```sh
+send -from FROM -to TO -amount AMOUNT
+```
+
+### Create Wallet
+Create a new wallet.
+```sh
+createwallet
+```
+
+### List Addresses
+List all addresses stored in the wallet file.
+```sh
+listaddresses
+```
+
+### Reindex Unspent Transactions
+Rebuild the UTXO set.
+```sh
+reindexutxo
+```
--- commandline/commandline.go
+++ commandline/commandline.go
@@ -22,6 +22,7 @@ func (cli *CommandLine) printUsage() {
 	fmt.Println("printchain - Prints the blocks in the chain")
 	fmt.Println("send -from FROM -to TO -amount AMOUNT - Send amount of coins")
 	fmt.Println("createwallet - Creates a new Wallet")
+	fmt.Println("reindexutxo - Rebuilds the UTXO set")
 	fmt.Println("listaddresses - List the addresses in our wallet file")
 }
 
@@ -30,6 +31,16 @@ func (cli *CommandLine) validateArgs() {
 		cli.printUsage()
 		runtime.Goexit()
 	}
+}
+
+func (cli *CommandLine) reindexUTXO() {
+	chain := blockchain.ContinueBlockChain("")
+	defer chain.Database.Close()
+	UTXOSet := blockchain.UTXOSet{chain}
+	UTXOSet.Reindex()
+
+	count := UTXOSet.CountTransactions()
+	fmt.Printf("Done! There are %d transactions in the UTXO set.\n", count)
 }
 
 func (cli *CommandLine) listAddresses() {
@@ -78,6 +89,10 @@ func (cli *CommandLine) createBlockChain(address string) {
 
 	chain := blockchain.InitBlockChain(address)
 	chain.Database.Close()
+
+	UTXOSet := blockchain.UTXOSet{chain}
+	UTXOSet.Reindex()
+
 	fmt.Println("Finished!")
 }
 
@@ -87,12 +102,13 @@ func (cli *CommandLine) getBalance(address string) {
 	}
 
 	chain := blockchain.ContinueBlockChain(address)
+	UTXOSet := blockchain.UTXOSet{chain}
 	defer chain.Database.Close()
 
 	balance := 0
 	pubKeyHash := wallet.Base58Decode([]byte(address))
 	pubKeyHash = pubKeyHash[1 : len(pubKeyHash)-4]
-	UTXOs := chain.FindUTXO(pubKeyHash)
+	UTXOs := UTXOSet.FindUTXO(pubKeyHash)
 
 	for _, out := range UTXOs {
 		balance += out.Value
@@ -110,10 +126,12 @@ func (cli *CommandLine) send(from, to string, amount int) {
 	}
 
 	chain := blockchain.ContinueBlockChain(from)
+	UTXOSet := blockchain.UTXOSet{chain}
 	defer chain.Database.Close()
 
-	tx := blockchain.NewTransaction(from, to, amount, chain)
-	chain.AddBlock([]*blockchain.Transaction{tx})
+	tx := blockchain.NewTransaction(from, to, amount, &UTXOSet)
+	block := chain.AddBlock([]*blockchain.Transaction{tx})
+	UTXOSet.Update(block)
 	fmt.Println("Success!")
 }
 
@@ -126,6 +144,7 @@ func (cli *CommandLine) Run() {
 	printChainCmd := flag.NewFlagSet("printchain", flag.ExitOnError)
 	createWalletCmd := flag.NewFlagSet("createwallet", flag.ExitOnError)
 	listAddressesCmd := flag.NewFlagSet("listaddresses", flag.ExitOnError)
+	reindexUTXOCmd := flag.NewFlagSet("reindexutxo", flag.ExitOnError)
 
 	getBalanceAddress := getBalanceCmd.String("address", "", "The address to get balance for")
 	createBlockChainAddress := createBlockChainCmd.String("address", "", "The address to send genesis block reward to")
@@ -151,6 +170,9 @@ func (cli *CommandLine) Run() {
 		blockchain.Handle(err)
 	case "listaddresses":
 		err := listAddressesCmd.Parse(os.Args[2:])
+		blockchain.Handle(err)
+	case "reindexutxo":
+		err := reindexUTXOCmd.Parse(os.Args[2:])
 		blockchain.Handle(err)
 	default:
 		cli.printUsage()
@@ -185,6 +207,10 @@ func (cli *CommandLine) Run() {
 
 	if listAddressesCmd.Parsed() {
 		cli.listAddresses()
+	}
+
+	if reindexUTXOCmd.Parsed() {
+		cli.reindexUTXO()
 	}
 
 	if sendCmd.Parsed() {
